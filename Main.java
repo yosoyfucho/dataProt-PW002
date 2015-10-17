5b0853f39f2b068c32a4132a05c53b4b3a1f3ab9
Laura B <laubenmar@gmail.com>
2015-10-17 13:57:28 +0200

    Main con comprobaciones correctas. Funciona!

--- Main.java
+++ Main.java
@@ -13,6 +13,8 @@ import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.security.Signature;
+import java.util.Arrays;
+
 import javax.crypto.Cipher;
 
 
@@ -22,7 +24,7 @@ public class Main{
 
 		PublicKey publicKey = null;
 		PrivateKey privateKey = null;
-		String plaintextString = "Esto es una prueba";
+		String plaintextString = "Hola Rafa, esto es un mensaje super secreto";
 		byte[] plaintext = null;
 		byte[] ciphertext = null;
 		byte[] decryptedPlaintext = null;
@@ -32,24 +34,22 @@ public class Main{
 		RSALibrary rsa = new RSALibrary();
 		
 		plaintext = plaintextString.getBytes();
-		System.out.println(plaintext);
-			
-		String executionPath = System.getProperty("user.dir");
+		
 		try{
 			
 			rsa.generateKeys();
 			
 			//comprobar que existen los ficheros tras generar las claves
 			
-			//File filePublicKey = new File(executionPath + "/public.key");
-			//File filePrivateKey = new File(executionPath + "/private.key");
-			
 			File filePublicKey = new File("./public.key");
 			File filePrivateKey = new File("./private.key");
+			File prueba = new File("./prueba.txt");
+			
 		
 			//Si existe el fichero de clave publica, la obtenemos y encriptamos
 			if(filePublicKey.exists() && !filePublicKey.isDirectory()) 
 			{ 
+				System.out.println("Existe public.key");
 				FileInputStream fileInput = new FileInputStream(filePublicKey);
 				ObjectInputStream objectInputStream = new ObjectInputStream(fileInput);
 				publicKey = (PublicKey) objectInputStream.readObject();
@@ -65,6 +65,7 @@ public class Main{
 			//Si existe el fichero de clave privada, la obtenemos y desencriptamos
 			if(filePrivateKey.exists() && !filePrivateKey.isDirectory()) 
 			{ 
+				System.out.println("Existe private.key");
 				FileInputStream fileInput = new FileInputStream(filePrivateKey);
 				ObjectInputStream objectInputStream = new ObjectInputStream(fileInput);
 				privateKey = (PrivateKey) objectInputStream.readObject();
@@ -77,16 +78,21 @@ public class Main{
 				System.out.println("Archivo de clave privada no creado");
 			}
 		
-			System.out.println("Plaintext original: " + plaintext.toString());
-			System.out.println("Plaintext desencriptado: " + decryptedPlaintext.toString());
+			System.out.println("Plaintext original:      " + plaintextString);
+			String ciphertextString = new String(ciphertext);
+			System.out.println("Texto cifrado:      " + ciphertextString);	
+			String decryptedPlaintextString = new String(decryptedPlaintext);
+			System.out.println("Plaintext desencriptado: " + decryptedPlaintextString);
+			
 			
 			System.out.println("Ahora a firmar ! ");
 			
 			byte[] signatureToBeVerified = rsa.sign(plaintext, privateKey);
-		
+			System.out.println("Firma: " + signatureToBeVerified.toString());
+			
 			isVerified = rsa.verify(plaintext, signatureToBeVerified, publicKey);
 		
-			System.out.println(isVerified);
+			System.out.println("¿La firma es correcta? : " + isVerified);
 		
 		}
 		catch(IOException | ClassNotFoundException e)
